a9cb1db9f2347a4331ff8b2522067bf2d5b4b2aa
Helder Correia <helder.patrick25@gmail.com>
2022-07-02 13:48:21 +0100

    Add functionality

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,38 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const user = { ...receivedUser, admin: true, updated_at: new Date() };
+    const userIndex = this.users.findIndex(
+      (user) => user.email === receivedUser.email
+    );
+    this.users.splice(userIndex, 1);
+    this.users.push(user);
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return [...this.users];
   }
 }
 
